d56cafd77ca5023b440b01161f6a8d40c7cfda3e
Christopher Aberger <caberger@Christophers-MacBook-Pro-2.local>
2015-10-10 23:47:00 -0700

    Added missing timer file.

---
+++ storage_engine/src/utils/timer.hpp
@@ -0,0 +1,23 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <iostream>
+#include <chrono>
+
+namespace timer{
+  static struct timeval tim; 
+  static std::chrono::time_point<std::chrono::system_clock> start_clock (){
+    return std::chrono::system_clock::now(); 
+  }
+
+  static inline double stop_clock(std::chrono::time_point<std::chrono::system_clock> t_in){
+    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now()-t_in; 
+    return elapsed_seconds.count();
+  }
+  static double stop_clock(std::string in,std::chrono::time_point<std::chrono::system_clock> t_in){
+    double t2= stop_clock(t_in);
+    std::cout << "Time["+in+"]: " << t2 << " s" << std::endl;
+    return t2;
+  }
+}
+#endif
