d81c611084b780dddf938b7da32dd90236b10bf4
Christopher Aberger <caberger@DNab42105f.stanford.edu>
2015-10-25 20:43:53 -0700

    Added in branch hack for apple machine.

--- tests/examples/triangle_materialized.py
+++ tests/examples/triangle_materialized.py
@@ -6,7 +6,7 @@ def main():
 	emptyheaded.loadDB("$EMPTYHEADED_HOME/examples/graph/data/facebook/db_pruned")
 	emptyheaded.query("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c).")
 
-	print numRows("Triangle")
-	print fetchData("Triangle")[0]
+	print emptyheaded.numRows("Triangle")
+	print emptyheaded.fetchData("Triangle")[0]
 
 if __name__ == "__main__": main()
