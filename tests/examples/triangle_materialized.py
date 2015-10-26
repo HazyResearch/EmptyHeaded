d08f0767527bf7e3e3c56afe1bc1de50535eab81
Christopher Aberger <caberger@DNab42105f.stanford.edu>
2015-10-25 18:49:41 -0700

    Added missing library calls to test.

--- tests/examples/triangle_materialized.py
+++ tests/examples/triangle_materialized.py
@@ -2,9 +2,9 @@ import emptyheaded
 
 def main():
 	db_config="$EMPTYHEADED_HOME/examples/graph/data/facebook/config_pruned.json"
-	createDB(db_config)
-	loadDB("$EMPTYHEADED_HOME/examples/graph/data/facebook/db_pruned")
-	query("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c).")
+	emptyheaded.createDB(db_config)
+	emptyheaded.loadDB("$EMPTYHEADED_HOME/examples/graph/data/facebook/db_pruned")
+	emptyheaded.query("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c).")
 
 	print numRows("Triangle")
 	print fetchData("Triangle")[0]
