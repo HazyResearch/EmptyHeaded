404692049fe4bce248193becf299ef49252a8f4f
Christopher Aberger <caberger@DNab40466d.stanford.edu>
2015-10-26 18:47:18 -0700

    Works on my mac as well.

--- runtime/wrapper/querywrapper.hpp
+++ runtime/wrapper/querywrapper.hpp
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "Query_loadDB.hpp"
+#include "Query_build_Edge.hpp"
 
-static PyObject * run_loadDB(PyObject * self, PyObject * args);
-static PyObject * num_rows_loadDB(PyObject * self, PyObject * args);
-static PyObject * fetch_data_loadDB(PyObject * self, PyObject * args);
+static PyObject * run_build_Edge(PyObject * self, PyObject * args);
+static PyObject * num_rows_build_Edge(PyObject * self, PyObject * args);
+static PyObject * fetch_data_build_Edge(PyObject * self, PyObject * args);
 
 static PyMethodDef EmptyHeadedQueryMethods[] = {
-  { "run_loadDB", run_loadDB, METH_VARARGS, "" },
-  { "num_rows_loadDB", num_rows_loadDB, METH_VARARGS, "Number of rows in result." },
-  { "fetch_data_loadDB", fetch_data_loadDB, METH_VARARGS, "The table in python." },
+  { "run_build_Edge", run_build_Edge, METH_VARARGS, "" },
+  { "num_rows_build_Edge", num_rows_build_Edge, METH_VARARGS, "Number of rows in result." },
+  { "fetch_data_build_Edge", fetch_data_build_Edge, METH_VARARGS, "The table in python." },
   { NULL, NULL, 0, NULL }
 };
