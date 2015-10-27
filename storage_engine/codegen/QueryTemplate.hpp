e40c2c0f4878f5ad752d9ff24f656a8044908ad8
Christopher Aberger <caberger@DNab40466d.stanford.edu>
2015-10-26 18:51:23 -0700

    Added template files.

---
+++ storage_engine/codegen/QueryTemplate.hpp
@@ -0,0 +1,34 @@
+#ifndef _QUERY_HASHSTRING_H_
+#define _QUERY_HASHSTRING_H_
+
+#include <vector>
+#include <stdint.h>
+#include <tuple>
+#include "Trie.hpp"
+#include "Encoding.hpp"
+
+#ifdef EXECUTABLE
+#include "main.hpp"
+#else
+struct application{};
+#endif
+
+struct ParMMapBuffer;
+struct ParMemoryBuffer;
+
+//template types are the types of the attributes, followed by the type of the annotation
+struct Query_HASHSTRING : public application {
+  void* result_HASHSTRING;
+
+	Query_HASHSTRING(){}
+	void run_HASHSTRING();
+  void fetch_result_HASHSTRING(); //TO DO implement something that iterates over the trie an returns the tuples
+};
+
+#ifdef EXECUTABLE
+application* init_app(){
+  return new Query_HASHSTRING(); 
+}
+#endif
+
+#endif
