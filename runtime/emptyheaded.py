c683399cbf8a12f1717a85c64d47d16fa955980f
Susan Tu <susanctu@raiders1.stanford.edu>
2015-10-28 00:00:46 -0700

    iadjust emptyheaded.query now that we use sbt pack

--- runtime/emptyheaded.py
+++ runtime/emptyheaded.py
@@ -10,14 +10,14 @@ import os
 hashindex = 0
 environment = codegenerator.env.Environment()
 
-QUERY_COMPILER_JAR = "target/scala-2.11/query_compiler-assembly-0.1-SNAPSHOT.jar"
+QUERY_COMPILER_RUN_SCRIPT = "target/pack/bin/query-compiler""
 
 def query(datalog_string):
   global hashindex
   qcpath = os.path.expandvars("$EMPTYHEADED_HOME")+"/query_compiler/"
   mydir=os.getcwd()
   os.chdir(qcpath)
-  subprocess.Popen("java -jar %s  -c %s/config.json \"%s\"" % (QUERY_COMPILER_JAR, environment.config["database"],datalog_string), cwd='../query_compiler' ,shell=True, stdout=subprocess.PIPE).stdout.read()
+  subprocess.Popen("%s  -c %s/config.json \"%s\"" % (QUERY_COMPILER_RUN_SCRIPT, environment.config["database"],datalog_string), cwd='../query_compiler' ,shell=True, stdout=subprocess.PIPE).stdout.read()
   os.chdir(mydir)
   environment.fromJSON(environment.config["database"]+"/config.json")
   
