619730bfa2c078d8312a156263b53adc69e1d4d8
Christopher Aberger <caberger@DNab404671.stanford.edu>
2015-11-09 14:52:49 -0800

    Added back to old test ways.

--- tests/examples/triangle_materialized.py
+++ tests/examples/triangle_materialized.py
@@ -4,6 +4,8 @@ class ResultError(Exception):
     pass
 
 def main():
+  db_config="$EMPTYHEADED_HOME/examples/graph/data/facebook/config_pruned.json"
+  emptyheaded.createDB(db_config)
 	emptyheaded.loadDB("$EMPTYHEADED_HOME/examples/graph/data/facebook/db_pruned")
 	emptyheaded.query("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c).")
 
