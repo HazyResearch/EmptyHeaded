1b6c780c00657369e0e6644ec5be64f42c9492a0
craberger <craberger@gmail.com>
2015-11-09 15:19:25 -0800

    Added missing test file.

--- tests/testAll.sh
+++ tests/testAll.sh
@@ -2,7 +2,7 @@ set -e
 source setup.sh
 
 #whole system tests
-#python tests/examples/create_testDB.py
+python tests/examples/create_testDB.py
 python tests/examples/triangle_materialized.py
 #python tests/examples/lollipop_agg.py
 #python tests/examples/barbell_agg.py
