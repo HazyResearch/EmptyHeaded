724db80b309cdcb266159bf6389ac3e0ec05443a
Christopher Aberger <caberger@DNab4213fd.stanford.edu>
2015-11-10 11:22:39 -0800

    Added in multi-threading.

--- runtime/codegenerator/cppgenerator.py
+++ runtime/codegenerator/cppgenerator.py
@@ -10,7 +10,7 @@ def generate(f,hashstring):
   cppfile.close()
   os.system("clang-format -style=llvm -i "+os.environ["EMPTYHEADED_HOME"]+"/storage_engine/codegen/Query.cpp")
 
-def compileC(hashstring):
+def compileC(hashstring,numThreads):
   command = """sed -e "s/HASHSTRING/"""+hashstring+"""/g" """+os.environ["EMPTYHEADED_HOME"]+"/storage_engine/codegen/Query.cpp > "+os.environ["EMPTYHEADED_HOME"]+"/storage_engine/generated/Query_"+hashstring+".cpp"
   os.system(command)
   os.system("rm -rf "+os.environ["EMPTYHEADED_HOME"]+"/storage_engine/codegen/Query.cpp")
@@ -19,22 +19,22 @@ def compileC(hashstring):
   #compile C++ code
   os.chdir(os.environ["EMPTYHEADED_HOME"]+"/storage_engine")
   try:
-    os.system("make Query_"+hashstring)#+" >/dev/null")
+    os.system("make NUM_THREADS="+numThreads+" Query_"+hashstring)#+" >/dev/null")
   except:
     print "C++ compilation failed"
     sys.exit(1)
 
   os.chdir(os.environ["EMPTYHEADED_HOME"]+"/runtime")
 
-def compileAndRun(f,hashstring,mem,types,annotationType):
+def compileAndRun(f,hashstring,mem,types,annotationType,numThreads):
 	generate(f,hashstring) #generates C++ code
-	compileC(hashstring) #compiles C++ library
+	compileC(hashstring,numThreads) #compiles C++ library
 	return cppexecutor.execute(hashstring,mem,types,annotationType) 	#generates python wrapper and excutes C++ code
 
 def run(f,name,mem,types,annotationType):
 	return cppexecutor.run(name,mem,types,annotationType) 	#generates python wrapper and excutes C++ code
 
-def compile(f,name,mem,types,annotationType):
+def compile(f,name,mem,types,annotationType,numThreads):
 	generate(f,name,hashstring) #generates C++ code
-	compileC(hashstring) #compiles C++ library
+	compileC(hashstring,numThreads) #compiles C++ library
 	return cppexecutor.compile(name,mem,types,annotationType) 	#generates python wrapper and excutes C++ code
