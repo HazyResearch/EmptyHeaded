160be662ad57982fc7798da19751f6c6c6e11c38
Christopher Aberger <caberger@DNab404682.stanford.edu>
2015-11-10 18:24:07 -0800

    Added missing file.

---
+++ tests/examples/4clique_agg.py
@@ -0,0 +1,14 @@
+import emptyheaded 
+
+class ResultError(Exception):
+    pass
+
+def main():
+  emptyheaded.loadDB("$EMPTYHEADED_HOME/examples/graph/data/facebook/db_pruned")
+  emptyheaded.query("Flique(;m:long) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),Edge(b,d),Edge(c,d);m=<<COUNT(*)>>.")
+
+  data = emptyheaded.fetchData("Flique")
+  if data[0][0] != 30004668L:
+    raise ResultError("NUMBER OF ROWS INCORRECT: " + str(data[0]))
+
+if __name__ == "__main__": main()
