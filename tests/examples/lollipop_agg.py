33b6b141e567ccc9b23166a73cc45aa22d3a6441
Christopher Aberger <caberger@Christophers-MacBook-Pro-2.local>
2015-11-11 19:33:24 -0800

    Want to try travis on new infra.

--- tests/examples/lollipop_agg.py
+++ tests/examples/lollipop_agg.py
@@ -8,7 +8,7 @@ def main():
   emptyheaded.query("Lollipop(;m:long) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,x);m=<<COUNT(*)>>.")
 
   data = emptyheaded.fetchData("Lollipop")
-  if data[0][0] != 1426911480L:
+  if data.iloc[0][0] != 1426911480L:
     raise ResultError("NUMBER OF ROWS INCORRECT: " + str(data[0]))
 
 if __name__ == "__main__": main()
