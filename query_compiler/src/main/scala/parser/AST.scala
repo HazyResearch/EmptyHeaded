6e8b4d584f4c0ff2db95e42a25d2a9f38c02b7e2
Susan Tu <susanctu@gmail.com>
2015-11-30 22:09:19 -0800

    separating out parser, env changes from failed recursion changes

--- query_compiler/src/main/scala/parser/AST.scala
+++ query_compiler/src/main/scala/parser/AST.scala
@@ -1,34 +1,24 @@
 package DunceCap
 
-abstract trait ASTStatement {
-}
+abstract trait ASTStatement
+abstract trait ASTConvergenceCondition
 
-//input to this should be 
-//(1) list of attrs in the output, 
-//(2) list of attrs eliminated (aggregations), 
-//(3) list of relations joined
-//(4) list of attrs with selections
-//(5) list of exressions for aggregations
-class ASTLambdaFunction(val inputArgument:QueryRelation,
-                        val join:List[QueryRelation],
-                        val aggregates:Map[String,ParsedAggregate])
+case class ASTItersCondition(iters:Int) extends ASTConvergenceCondition
+case class ASTEpsilonCondition(eps:Double) extends ASTConvergenceCondition
+case class InfiniteRecursionException(what:String) extends Exception
 
-case class ASTQueryStatement(
-                              lhs:QueryRelation,
-                              joinType:String,
-                              join:List[QueryRelation],
-                              recursion:Option[RecursionStatement],
-                              tc:Option[TransitiveClosureStatement],
-                              joinAggregates:Map[String,ParsedAggregate]) extends ASTStatement {
+case class ASTQueryStatement(lhs:QueryRelation,
+                             convergence:Option[ASTConvergenceCondition],
+                             joinType:String,
+                             join:List[QueryRelation],
+                             joinAggregates:Map[String,ParsedAggregate]) extends ASTStatement {
   // TODO (sctu) : ignoring everything except for join, joinAggregates for now
 
-  def computePlan(config:Config): QueryPlan = {
-    // get the annotations
-    val missingRelations = join.filter(rel => !Environment.isLoaded(rel))
-    if (!missingRelations.isEmpty) {
-      throw new RelationNotFoundException("TODO: fill in with a better explanation")
+  def computePlan(config:Config, isRecursive:Boolean): QueryPlan = {
+    val annotationSetSuccess = join.map(rel => Environment.setAnnotationAccordingToConfig(rel))
+    if (annotationSetSuccess.find(b => !b).isDefined) {
+      throw RelationNotFoundException("TODO: fill in with a better explanation")
     }
-    val joinQueryWithAnnotations = join.map(rel => Environment.setAnnotationAccordingToConfig(rel))
 
     if (!config.nprrOnly) {
       val rootNodes = GHDSolver.getMinFHWDecompositions(join);
