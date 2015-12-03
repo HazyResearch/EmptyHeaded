aad5573cf8fcda21829f92e4814d11a409350e2c
craberger <craberger@gmail.com>
2015-12-02 20:44:26 -0800

    Almost fixed SSSP.

--- storage_engine/src/TransitiveClosure.hpp
+++ storage_engine/src/TransitiveClosure.hpp
@@ -23,7 +23,7 @@ namespace tc {
     ///just stuff to setup the visited set
     const size_t visited_num_bytes = sizeof(TrieBlock<range_bitset,M>) +
       sizeof(Set<range_bitset>) +
-      range_bitset::get_number_of_bytes(num_distinct,num_distinct) +
+      PADDING*range_bitset::get_number_of_bytes(num_distinct,num_distinct) +
       (sizeof(A)*num_distinct);
 
     uint8_t* bs_data = output->memoryBuffers->get_next(NUM_THREADS,visited_num_bytes);
@@ -39,10 +39,10 @@ namespace tc {
 
     //setup buffers for the frontier
     uint32_t* frontier_allocator = new uint32_t[num_distinct*NUM_THREADS];
-    uint32_t** frontier_buffer = new uint32_t*[NUM_THREADS*PADDING];
+    uint32_t** frontier_buffer = new uint32_t*[NUM_THREADS];
     uint32_t* frontier_sizes = new uint32_t[NUM_THREADS*PADDING];
     for(size_t i = 0; i < NUM_THREADS; i++){
-      frontier_buffer[i*PADDING] = &frontier_allocator[i*num_distinct];
+      frontier_buffer[i] = &frontier_allocator[i*num_distinct];
       frontier_sizes[i*PADDING] = 0;
     }
 
@@ -53,30 +53,40 @@ namespace tc {
     //visited->set_data(0,start,0);
     frontier[0] = start;
 
+    uint64_t const * set_data = ((uint64_t*)vs->get_data())+1;
+    const TrieBlock<T,M> * input_head = input->getHead();
     size_t iteration = 0;
     while(frontier_size != 0){
       std::cout  << "ITERATION: " << iteration << " FRONTIER SIZE: " << frontier_size << std::endl;
-      par::for_range(0,frontier_size,100,[&](size_t tid, size_t i){
+      auto tt = timer::start_clock();
+      par::for_range(0,frontier_size,100,[&](const size_t tid, const size_t i){
         const uint32_t f = frontier[i];
-        TrieBlock<T,M>* level2 = input->getHead()->get_next_block(f,input->memoryBuffers);
+        const TrieBlock<T,M>* level2 = input_head->get_next_block(f,input->memoryBuffers);
         if(level2 != NULL){
-          level2->get_const_set()->foreach([&](uint32_t l2){
+          level2->get_const_set()->foreach([&](const uint32_t l2){
             //union in element and return index if element does not exist in the set
             // and return -1 if it exists in the set (no work to do in the union)
-            if(ops::atomic_union(vs,l2)){
-              visited->template set_annotation<A>(join(visited->template get_annotation<A>(0,f)),0,l2); //index does not matter this is dense
-              const size_t buffer_index = frontier_sizes[tid*PADDING];
-              frontier_buffer[tid*PADDING][buffer_index] = l2;
-              frontier_sizes[tid*PADDING]++;
+            const size_t word = range_bitset::word_index(l2);
+            const uint64_t set_bit = ((uint64_t) 1 << (l2%BITS_PER_WORD));
+            if(!(set_data[word] & set_bit)){
+              const uint64_t old_value = __sync_fetch_and_or((uint64_t*)&set_data[word],set_bit);
+              if(!(old_value & set_bit)){
+                visited->template set_annotation<A>(join(visited->template get_annotation<A>(0,f)),0,l2); //index does not matter this is dense
+                const size_t buffer_index = frontier_sizes[tid*PADDING];
+                frontier_buffer[tid][buffer_index] = l2;
+                frontier_sizes[tid*PADDING]++;
+              }
             }
           });
         }
       });
+      timer::stop_clock("FRONTIER: ",tt);
+
       //reconstruct frontier buffer
       frontier_size = 0;
       for(size_t t = 0 ; t < NUM_THREADS; t++){
         const size_t num_thread_elems = frontier_sizes[t*PADDING];
-        memcpy(&frontier[frontier_size],frontier_buffer[t*PADDING],num_thread_elems*sizeof(uint32_t));
+        memcpy(&frontier[frontier_size],frontier_buffer[t],num_thread_elems*sizeof(uint32_t));
         frontier_size += num_thread_elems;
         frontier_sizes[t*PADDING] = 0;
       }
