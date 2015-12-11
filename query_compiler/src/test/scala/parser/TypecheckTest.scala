ad0db9c17b707ddb191c7935d7689049f4cc02d6
Susan Tu <susanctu@gmail.com>
2015-12-11 00:02:05 -0800

    should have gone into last commit

--- query_compiler/src/test/scala/parser/TypecheckTest.scala
+++ query_compiler/src/test/scala/parser/TypecheckTest.scala
@@ -6,6 +6,51 @@ import org.scalatest.FunSuite
 class TypecheckTest extends FunSuite {
   test("Throws exception when output attribute is not in the query body") {
     Environment.fromJSON("example_config/config.json") // load edge relation
+    val configContents =
+      """
+        |{
+        |    "algorithm": "NPRR+",
+        |    "database": "/dfs/scratch0/susanctu/newversion/EmptyHeaded/examples/graph/data/facebook/db_pruned",
+        |    "encodings": {
+        |        "node": "long"
+        |    },
+        |    "layout": "hybrid",
+        |    "memory": "ParMemoryBuffer",
+        |    "numNUMA": 4,
+        |    "numThreads": 4,
+        |    "relations": {
+        |        "Edge_0_1": "disk",
+        |        "Edge_1_0": "disk"
+        |    },
+        |    "resultName": "",
+        |    "resultOrdering": [],
+        |    "schemas": {
+        |        "Edge": {
+        |            "annotation": "void*",
+        |            "attributes": [
+        |                {
+        |                    "attrType": "long",
+        |                    "encoding": "node"
+        |                },
+        |                {
+        |                    "attrType": "long",
+        |                    "encoding": "node"
+        |                }
+        |            ],
+        |            "orderings": [
+        |                [
+        |                    0,
+        |                    1
+        |                ],
+        |                [
+        |                    1,
+        |                    0
+        |                ]
+        |            ]
+        |        }
+        |    }
+        |}
+      """.stripMargin
     val query = ASTQueryStatement(
         QueryRelation("lhs", List(("a", "", ""), ("d", "", ""))),
         None,
