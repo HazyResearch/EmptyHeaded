e71199b35282f26c9a7dd6ae8256ed6d6c103779
Christopher Aberger <craberger@gmail.com>
2016-01-06 14:36:05 -0800

    Patched bug.

--- storage_engine/src/TrieBuilder.cpp
+++ storage_engine/src/TrieBuilder.cpp
@@ -177,7 +177,8 @@ size_t TrieBuilder<A,M>::build_set(
       return 0;
     }
     Set<hybrid>* tmp_set = (Set<hybrid>*)((uint8_t*)tmp_head_set+sizeof(TrieBlock<hybrid,M>));
-    alloc_size = get_alloc_size(alloc_size,tmp_set->number_of_bytes,(same && (prev_t == tmp_set->type)));
+    same = same && (prev_t == tmp_set->type);
+    alloc_size = get_alloc_size(alloc_size,tmp_set->number_of_bytes,same);
     min_set = std::min((size_t)min_set,(size_t)tmp_set->cardinality);
     prev_t = tmp_set->type;
     if(min_set == tmp_set->cardinality){
@@ -617,7 +618,8 @@ size_t ParTrieBuilder<A,M>::build_set(
       return 0;
     }
     Set<hybrid>* tmp_set = (Set<hybrid>*)((uint8_t*)tmp_head_set+sizeof(TrieBlock<hybrid,M>));
-    alloc_size = get_alloc_size(alloc_size,tmp_set->number_of_bytes,(same && (prev_t == s1->type)));
+    same = same && (prev_t == s1->type);
+    alloc_size = get_alloc_size(alloc_size,tmp_set->number_of_bytes,);
     min_set = std::min((size_t)min_set,(size_t)tmp_set->cardinality);
     prev_t = s1->type;
     if(min_set == tmp_set->cardinality){
