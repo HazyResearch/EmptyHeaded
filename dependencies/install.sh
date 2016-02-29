93a7d77ba3018655c1cf23acb273e6eb5ce82879
Christopher Aberger <craberger@gmail.com>
2016-02-29 15:55:27 -0800

    Cleaned up install.

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,7 +1,4 @@
 tar -xvf JPype1-0.6.1.tar.gz
 cd JPype1-0.6.1
-#mkdir -p $EMPTYHEADED_HOME/dependencies/JPype/lib/python2.7/site-packages
-#export PYTHONPATH=$PYTHONPATH:$EMPTYHEADED_HOME/dependencies/JPype/lib/python2.7/site-packages
-#python setup.py install --prefix=$EMPTYHEADED_HOME/dependencies/JPype --user 
 python setup.py install --user
 cd -
