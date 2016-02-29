efe5705979cb658dea9cab6f62f76991b755dea1
Christopher Aberger <craberger@gmail.com>
2016-02-29 15:54:47 -0800

    Added new install for JPype..should be user level now.

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,4 +1,7 @@
 tar -xvf JPype1-0.6.1.tar.gz
 cd JPype1-0.6.1
-sudo python setup.py install
+#mkdir -p $EMPTYHEADED_HOME/dependencies/JPype/lib/python2.7/site-packages
+#export PYTHONPATH=$PYTHONPATH:$EMPTYHEADED_HOME/dependencies/JPype/lib/python2.7/site-packages
+#python setup.py install --prefix=$EMPTYHEADED_HOME/dependencies/JPype --user 
+python setup.py install --user
 cd -
