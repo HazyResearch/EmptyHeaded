e4803562e804f718e65c703ed13ee3497c7a47d2
Susan Tu <susanctu@gmail.com>
2016-02-29 15:26:18 -0800

    adjusting tests after chris's QueryPlanner API change

--- query_compiler/src/test/scala/QueryPlannerTest.scala
+++ query_compiler/src/test/scala/QueryPlannerTest.scala
@@ -25,7 +25,8 @@ class QueryPlannerTest extends FunSuite {
     )
     val ir = IR(List(rule))
 
-    val optimized = QueryPlanner.findOptimizedPlans("Simple(a,b) :- Edge(a,b).")
+    val optimized = QueryPlanner.findOptimizedPlans(
+      DatalogParser.run("Simple(a,b) :- Edge(a,b)."))
     assertResult(result)(optimized.rules.head.result)
     assertResult(operation)(optimized.rules.head.operation)
     assertResult(order)(optimized.rules.head.order)
@@ -50,7 +51,7 @@ class QueryPlannerTest extends FunSuite {
       Filters(List())
     )))
 
-    val optimized = QueryPlanner.findOptimizedPlans("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c).")
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run("Triangle(a,b,c) :- Edge(a,b),Edge(b,c),Edge(a,c)."))
     assertResult(optimized)(ir)
   }
 
@@ -95,8 +96,8 @@ class QueryPlannerTest extends FunSuite {
       Filters(List())
     )
 
-    val optimized = QueryPlanner.findOptimizedPlans("Lollipop(a,b,c,d) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d).")
-    assertResult(IR(List(topdownPass, bag0, bag1)))(optimized)
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run("Lollipop(a,b,c,d) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d)."))
+    assertResult(IR(List(bag1, bag0, topdownPass)))(optimized)
   }
 
   test("barbell query") {
@@ -159,36 +160,42 @@ class QueryPlannerTest extends FunSuite {
       Filters(List())
     )
 
-    val optimized = QueryPlanner.findOptimizedPlans(
-      "Barbell(a,b,c,d,e,f) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(d,e),Edge(e,f),Edge(d,f),Edge(a,d).")
-    assertResult(topdownPass)(optimized.rules(0))
-    assertResult(bag0)(optimized.rules(1))
-    assertResult(bag1)(optimized.rules(2))
-    assertResult(bag2)(optimized.rules(3))
-    assertResult(IR(List(topdownPass, bag0, bag1, bag2)))(optimized)
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run(
+      "Barbell(a,b,c,d,e,f) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(d,e),Edge(e,f),Edge(d,f),Edge(a,d)."))
+    assertResult(topdownPass)(optimized.rules(3))
+    assertResult(bag0)(optimized.rules(2))
+    assertResult(bag1)(optimized.rules(1))
+    assertResult(bag2)(optimized.rules(0))
+    assertResult(IR(List(bag2, bag1, bag0, topdownPass)))(optimized)
   }
 
   test("lollipop agg") {
     val ir = IR(List(
-      Rule(Result(
-        Rel("Lollipop",Attributes(List()),Annotations(List("z"))), false),None,Operation("*"),Order(Attributes(List("a", "d"))),Project(Attributes(List())),
-        Join(List(
-          Rel("Edge",Attributes(List("a", "d")),Annotations(List())),
-          Rel("bag_1_a_b_c_Lollipop",Attributes(List("a")),Annotations(List("z"))))),
-        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("a", "d")),"1","AGG"))),Filters(List())),
       Rule(Result(
         Rel("bag_1_a_b_c_Lollipop",Attributes(List("a")),Annotations(List("z"))), true),None,Operation("*"),Order(Attributes(List("a", "b", "c"))),Project(Attributes(List())),
         Join(List(
           Rel("Edge",Attributes(List("a", "c")),Annotations(List())),
           Rel("Edge",Attributes(List("b", "c")),Annotations(List())),
           Rel("Edge",Attributes(List("a", "b")),Annotations(List())))),
-        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("b", "c")),"1","AGG"))),Filters(List()))))
-    val optimized = QueryPlanner.findOptimizedPlans("Lollipop(;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),z:uint64<-[COUNT(*)]")
+        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("b", "c")),"1","AGG"))),Filters(List())),
+      Rule(Result(
+        Rel("Lollipop",Attributes(List()),Annotations(List("z"))), false),None,Operation("*"),Order(Attributes(List("a", "d"))),Project(Attributes(List())),
+        Join(List(
+          Rel("Edge",Attributes(List("a", "d")),Annotations(List())),
+          Rel("bag_1_a_b_c_Lollipop",Attributes(List("a")),Annotations(List("z"))))),
+        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("a", "d")),"1","AGG"))),Filters(List()))
+      ))
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run("Lollipop(;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),z:uint64<-[COUNT(*)]"))
     assertResult(ir)(optimized)
   }
 
   test("lollipop partial agg with project") {
     val ir = IR(List(
+      Rule(Result(
+        Rel("bag_1_a_d_Lollipop",Attributes(List("a")),Annotations(List("z"))), true),None,Operation("*"),Order(Attributes(List("a", "d"))),Project(Attributes(List())),
+        Join(List(
+          Rel("Edge", Attributes(List("a", "d")),Annotations(List())))),
+        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("d")),"1","AGG"))),Filters(List())),
       Rule(Result(
         Rel("Lollipop",Attributes(List("a")),Annotations(List("z"))), false),None,Operation("*"),Order(Attributes(List("a", "b", "c"))),Project(Attributes(List("b"))),
         Join(List(
@@ -196,19 +203,22 @@ class QueryPlannerTest extends FunSuite {
           Rel("Edge",Attributes(List("b", "c")),Annotations(List())),
           Rel("Edge",Attributes(List("a", "b")),Annotations(List())),
           Rel("bag_1_a_d_Lollipop",Attributes(List("a")),Annotations(List("z"))))),
-        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("c")),"1","AGG"))),Filters(List())),
-      Rule(Result(
-        Rel("bag_1_a_d_Lollipop",Attributes(List("a")),Annotations(List("z"))), true),None,Operation("*"),Order(Attributes(List("a", "d"))),Project(Attributes(List())),
-        Join(List(
-          Rel("Edge", Attributes(List("a", "d")),Annotations(List())))),
-        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("d")),"1","AGG"))),Filters(List()))))
-    val optimized = QueryPlanner.findOptimizedPlans(
-      "Lollipop(a;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),z:uint64<-[COUNT(c,d)]")
+        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("c")),"1","AGG"))),Filters(List()))))
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run(
+      "Lollipop(a;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),z:uint64<-[COUNT(c,d)]"))
     assertResult(ir)(optimized)
   }
 
   test("4 clique with selection and aggregation") {
     val ir = IR(List(
+      Rule(Result(
+        Rel("bag_1_x_a_FliqueSelAgg",Attributes(List("a")),Annotations(List("z"))),true),
+        None,
+        Operation("*"),
+        Order(Attributes(List("x", "a"))),
+        Project(Attributes(List("x"))),
+        Join(List(Rel("Edge",Attributes(List("a", "x")),Annotations(List())))),
+        Aggregations(List()),Filters(List(Selection("x",EQUALS(),"0")))),
       Rule(Result(
         Rel("FliqueSelAgg",Attributes(List()),Annotations(List("z"))),false),
         None,
@@ -223,27 +233,20 @@ class QueryPlannerTest extends FunSuite {
           Rel("bag_1_x_a_FliqueSelAgg",Attributes(List("a")),Annotations(List("z"))),
           Rel("Edge",Attributes(List("a", "d")),Annotations(List())),
           Rel("Edge",Attributes(List("a", "b")),Annotations(List())))),
-        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("a", "b", "c", "d")),"1","AGG"))),Filters(List())),
-      Rule(Result(
-        Rel("bag_1_x_a_FliqueSelAgg",Attributes(List("a")),Annotations(List("z"))),true),
-        None,
-        Operation("*"),
-        Order(Attributes(List("x", "a"))),
-        Project(Attributes(List("x"))),
-        Join(List(Rel("Edge",Attributes(List("a", "x")),Annotations(List())))),
-        Aggregations(List()),Filters(List(Selection("x",EQUALS(),"0"))))))
+        Aggregations(List(Aggregation("z","long",SUM(),Attributes(List("a", "b", "c", "d")),"1","AGG"))),Filters(List()))
+      ))
 
-    val optimized = QueryPlanner.findOptimizedPlans(
-      "FliqueSelAgg(;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),Edge(b,d),Edge(c,d),Edge(a,x),x=0,z:uint64<-[COUNT(*)].")
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run(
+      "FliqueSelAgg(;z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,d),Edge(b,d),Edge(c,d),Edge(a,x),x=0,z:uint64<-[COUNT(*)]."))
     assertResult(ir)(optimized)
     //println(optimized)
   }
 
   test("Barbell with selection") {
     // TODO
-    val optimized = QueryPlanner.findOptimizedPlans("""
+    val optimized = QueryPlanner.findOptimizedPlans(DatalogParser.run("""
         BarbellSel(a,b,c,x,y,z) :- Edge(a,b),Edge(b,c),Edge(a,c),Edge(a,p),Edge(p,x),Edge(x,y),Edge(y,z),Edge(x,z),p=0.
-    """)
+    """))
     println(optimized)
   }
 }
