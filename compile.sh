61d3594c536389e04156c7bea39fcf7a4494cb1e
Christopher Aberger <craberger@gmail.com>
2016-02-29 16:31:31 -0800

    Added new

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
 set e
-cd $EMPTYHEADED_HOME/query_compiler && sbt pack && cd -
-
+cd $EMPTYHEADED_HOME/query_compiler 
+sbt pack
+cd - 
 #this is static
-cd $EMPTYHEADED_HOME/cython/db && ./build.sh >compilation.log 2>&1 && mv *.so $EMPTYHEADED_HOME/python/ && cd -
+cd $EMPTYHEADED_HOME/cython/db
+./build.sh 
+mv *.so $EMPTYHEADED_HOME/python/ 
+cd -
